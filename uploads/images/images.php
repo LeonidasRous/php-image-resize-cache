31e3441fa6a263261e8dde8695d989175855288e
Leonidas Rousias <leonidasrous89@gmail.com>
2018-06-26 13:59:54 +0300

    ADD comments

--- uploads/images/images.php
+++ uploads/images/images.php
@@ -1,56 +1,73 @@
 <?php
-	// The file
-//	include 'admin/common/common.php';
-	$filename = '../media/';
-	$filename = $filename.$_REQUEST['file'];
-	if(!file_exists($filename)){
-		$filename='../media/files/1/TV  Photo archive/tv_photos_980_400/enimerwsi/skailogo mikro.jpg';
+	// HERE IS THE PATH TO IMAGE FOLDER
+	$media_folder = '../media/';
+        // HERE IS THE PATH TO TMP FOLDER
+        $tmp_folder = 'tmp/';
+	// GET THE IMAGE PATH FROM URL AND CHECK IF IMAGE EXISTS
+	$filename = $media_folder.$_REQUEST['file'];
+	if( !file_exists( $filename ) )
+	{
+		// THIS IS THE DEFAULT IMAGE
+		$default_image = 'no_image.png'; 
+		$filename = $media_folder . $default_image;
 	}
-	$percent = 0.5; // percentage of resize
-//echo $filename;
-    // Content type
-	$ext=explode('.',$_REQUEST['file']);
-	$ext=$ext[count($ext)-1];
+	$ext = explode( '.' , $_REQUEST['file'] );
+	$ext = $ext[ count( $ext )-1 ];
+	$aa = getimagesize( $filename );
+	$width = $aa[0];
+	$height = $aa[1];
+	$mime = $aa['mime'];
 
-	$aa=getimagesize($filename);
-	//print_r($aa);
-	$width=$aa[0];
-	$height=$aa[1];
-	$mime=$aa[mime];
-
+	// HEADERS FROM CONTENT TYPE AND CACHE-CONTROL AND BROWSER CACHING
 	header('Pragma: public');
 	header('Cache-Control: max-age=604800');
 	header('Expires: '. gmdate('D, d M Y H \G\M\T', time() + 186400));
 	header('Content-type: '.$mime);
-
+	// HERE IS THE TEMPORARY IMAGE NAME THAT USED TO SAVE THE  
 	$tmpimagename=explode('/',$_REQUEST['file']);
 	$tmpimagename=implode('_',$tmpimagename);
 	$tmpimagename=$_REQUEST[hsize].'_'.$_REQUEST[wsize].'_'.$tmpimagename;
-	if(file_exists('tmp/'.$tmpimagename) && !isset($_REQUEST['decache'])){
-		$file=file_get_contents('tmp/'.$tmpimagename);
+	// CHECKING IF IMAGE EXISTS OR USE {img_url}?decache=1 TO REMAKE THE IMAGE
+	if( file_exists( $tmp_folder . $tmpimagename ) && ( !isset( $_REQUEST['decache'] || ( isset( $_REQUEST['decache'] ) && $_REQUEST['decache'] != 1 ) ) )
+   	{
+		// GET TMP IMAGE AND DISPLAY IT
+		$file = file_get_contents( $tmp_folder . $tmpimagename );
 		echo $file;
 		exit();
 	}
-	// Get new dimensions
-	//	list($width, $height,$a,$b,$c,$d,$e,$f,$mime) = getimagesize($filename);
-
-	//exit();
-	if($_REQUEST[hsize]>0 && $_REQUEST[wsize]>0) {
-		$new_width = $_REQUEST[wsize];
-		$new_height = $_REQUEST[hsize];
-	} else if($_REQUEST[hsize]>0 && $_REQUEST[wsize]==0){
-	    if ($width > $height) {
-	        $a = 100 * $_REQUEST[hsize];
-	        $b = $a / $width;
+	   
+	/*
+	*
+	*	CALCULATE THE SIZE OF THE IMAGE
+	* 	IF `hsize` AND `wsize` ARE BIGGER THAN ZERO THEN THE IMAGE STRETCHED TO EXACTLY THIS SIZE
+	*	ELSE IF ONE OF THEM IS ZERO THEN THE WIDTH OR HEIGHT OF THE IMAGE WILL BE THE SETTED SIZE AND THE OTHER WILL BE CALCULATED WITH THE IMAGE RATIO
+	*	ELSE IF BOTH OF THEM IS ZERO THE IMAGE WILL BE TO HIS ORIGINAL SIZE
+	*
+	*/
+	if( $_REQUEST['hsize'] > 0 && $_REQUEST['wsize'] > 0 )
+	{
+		$new_width = $_REQUEST['wsize'];
+		$new_height = $_REQUEST['hsize'];
+	} 
+   	else if( $_REQUEST['hsize'] > 0 && $_REQUEST['wsize'] == 0 )
+   	{
+		if ( $width > $height ) 
+		{
+			$a = 100 * $_REQUEST['hsize'];
+			$b = $a / $width;
 	        $b = $b / 100;
-	    } else {
+	    } 
+		else 
+		{
 	        $a = 100 * $_REQUEST['hsize'];
 	        $b = $a / $height;
 	        $b = $b / 100;
 	    }
 	    $new_width = $width * $b;
 		$new_height = $height * $b;
-	} else {
+	} 
+	else
+	{
 	    $b=1;
 		$new_width = $width * $b;
 		$new_height = $height * $b;
@@ -99,6 +116,6 @@
 			$image = false;
 			break;
 	}
-	$file=file_get_contents('tmp/'.$tmpimagename);
+	$file = file_get_contents( $tmp_folder . $tmpimagename );
 	echo $file;
 ?>
